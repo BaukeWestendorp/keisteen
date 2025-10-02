9bc3ce813f254cf69be7bd7625349a41b15adfc3
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-02 20:03:52 +0200

    Remove sending of play Login packet

--- src/server/conn/packet/config.rs
+++ src/server/conn/packet/config.rs
@@ -22,37 +22,6 @@ impl Connection {
             SConfigurationPacket::AcknowledgeFinishConfiguration => {
                 log::debug!("configuration acknowledged");
                 self.state = ConnectionState::Play;
-
-                self.send_packet(CPlayPacket::Login {
-                    entity_id: 1,
-                    is_hardcore: false,
-                    dimension_names: vec![Identifier::new(
-                        "minecraft".to_string(),
-                        "overworld".to_string(),
-                    )?],
-                    max_players: VarInt::new(20),
-                    view_distance: VarInt::new(10),
-                    simulation_distance: VarInt::new(10),
-                    reduced_debug_info: false,
-                    enable_respawn_screen: true,
-                    do_limited_crafting: false,
-                    dimension_type: VarInt::new(1),
-                    dimension_name: Identifier::new(
-                        "minecraft".to_string(),
-                        "overworld".to_string(),
-                    )?,
-                    hashed_seed: 0,
-                    game_mode: 1,
-                    previous_game_mode: -1,
-                    is_debug: false,
-                    is_flat: false,
-                    has_death_location: false,
-                    death_dimension: None,
-                    death_location: None,
-                    portal_cooldown: VarInt::new(0),
-                    sea_level: VarInt::new(63),
-                    enforces_secure_chat: false,
-                })?;
             }
             SConfigurationPacket::KeepAlive => todo!(),
             SConfigurationPacket::Pong => todo!(),
