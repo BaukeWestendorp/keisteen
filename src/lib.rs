03e96e468224514ed523595c13e03e357b0a7aa4
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-04 11:59:41 +0200

    Change MC_VERSION to 1.21.8

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use crate::types::VarInt;
 
 pub const BRAND: &str = "Keisteen";
-pub const MC_VERSION: &str = "1.18.2";
+pub const MC_VERSION: &str = "1.21.8";
 pub const MC_PROTOCOL: VarInt = VarInt::new(772);
 
 pub mod error;
