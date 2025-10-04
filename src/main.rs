599726d1142b75eac44d0564fc9bcedc295ebf84
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-04 17:02:49 +0200

    Replace `pretty_env_logger` with `env_logger`

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use keisteen::server::conn::ConnectionManager;
 use keisteen::server::{Server, ServerHandle};
 
 fn main() {
-    pretty_env_logger::init();
+    env_logger::builder().format_timestamp(None).init();
 
     let handle = ServerHandle::new(Server::new());
     ConnectionManager::new(handle).bind("127.0.0.1:25565").expect("should start server");
