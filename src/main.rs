2cff5ea156d4c4c4f4acf6df9a3de3f0cc5da220
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-07 01:12:55 +0200

    Bump Minecraft version to 1.21.9 and protocol to 773

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ pub(crate) mod mc;
 pub(crate) mod server;
 
 pub const BRAND: &str = "Keisteen";
-pub const MC_VERSION: &str = "1.21.8";
-pub const MC_PROTOCOL: VarInt = VarInt::new(772);
+pub const MC_VERSION: &str = "1.21.9";
+pub const MC_PROTOCOL: VarInt = VarInt::new(773);
 
 #[derive(Parser)]
 #[command(name = "Keisteen Server")]
