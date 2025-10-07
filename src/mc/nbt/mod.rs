0c964b56d1781faacb09e8866c73f990486eb7cb
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-08 00:18:02 +0200

    Refactor NBT module to use Vec for tag storage

--- src/mc/nbt/mod.rs
+++ src/mc/nbt/mod.rs
@@ -1,4 +1,8 @@
-use std::collections::HashMap;
+mod error;
+mod ser;
+
+pub use error::{Error, Result};
+pub use ser::{Serializer, to_nbt};
 
 pub const TAG_END: u8 = 0;
 pub const TAG_BYTE: u8 = 1;
@@ -14,12 +18,13 @@ pub const TAG_COMPOUND: u8 = 10;
 pub const TAG_INT_ARRAY: u8 = 11;
 pub const TAG_LONG_ARRAY: u8 = 12;
 
+#[derive(Debug, Clone)]
 pub struct Nbt {
     pub compound: CompoundTag,
 }
 
 impl Nbt {
-    pub fn as_bytes(&self) -> Vec<u8> {
+    pub fn as_named_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
         bytes.push(TAG_COMPOUND);
         write_tag_name_bytes(&self.compound.name, &mut bytes);
@@ -35,78 +40,80 @@ impl Nbt {
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct CompoundTag {
     pub name: String,
-    pub tags: HashMap<String, Tag>,
+    pub tags: Vec<(String, Tag)>,
 }
 
+#[derive(Debug, Clone)]
 pub enum Tag {
     End,
-    Byte { value: i8 },
-    Short { value: i16 },
-    Int { value: i32 },
-    Long { value: i64 },
-    Float { value: f32 },
-    Double { value: f64 },
-    ByteArray { value: Vec<i8> },
-    String { value: String },
-    List { tag_type: u8, value: Vec<Tag> },
-    Compound { value: HashMap<String, Tag> },
-    IntArray { value: Vec<i32> },
-    LongArray { value: Vec<i64> },
+    Byte(i8),
+    Short(i16),
+    Int(i32),
+    Long(i64),
+    Float(f32),
+    Double(f64),
+    ByteArray(Vec<i8>),
+    String(String),
+    List(u8, Vec<Tag>),
+    Compound(Vec<(String, Tag)>),
+    IntArray(Vec<i32>),
+    LongArray(Vec<i64>),
 }
 
 impl Tag {
-    pub fn tag_type(&self) -> u8 {
+    pub fn tag_type_id(&self) -> u8 {
         match self {
             Tag::End => TAG_END,
-            Tag::Byte { .. } => TAG_BYTE,
-            Tag::Short { .. } => TAG_SHORT,
-            Tag::Int { .. } => TAG_INT,
-            Tag::Long { .. } => TAG_LONG,
-            Tag::Float { .. } => TAG_FLOAT,
-            Tag::Double { .. } => TAG_DOUBLE,
-            Tag::ByteArray { .. } => TAG_BYTE_ARRAY,
-            Tag::String { .. } => TAG_STRING,
-            Tag::List { .. } => TAG_LIST,
-            Tag::Compound { .. } => TAG_COMPOUND,
-            Tag::IntArray { .. } => TAG_INT_ARRAY,
-            Tag::LongArray { .. } => TAG_LONG_ARRAY,
+            Tag::Byte(_) => TAG_BYTE,
+            Tag::Short(_) => TAG_SHORT,
+            Tag::Int(_) => TAG_INT,
+            Tag::Long(_) => TAG_LONG,
+            Tag::Float(_) => TAG_FLOAT,
+            Tag::Double(_) => TAG_DOUBLE,
+            Tag::ByteArray(_) => TAG_BYTE_ARRAY,
+            Tag::String(_) => TAG_STRING,
+            Tag::List(_, _) => TAG_LIST,
+            Tag::Compound(_) => TAG_COMPOUND,
+            Tag::IntArray(_) => TAG_INT_ARRAY,
+            Tag::LongArray(_) => TAG_LONG_ARRAY,
         }
     }
 
     pub fn write_bytes(&self, bytes: &mut Vec<u8>) {
         match self {
             Tag::End => {}
-            Tag::Byte { value } => bytes.extend_from_slice(&value.to_be_bytes()),
-            Tag::Short { value } => bytes.extend_from_slice(&value.to_be_bytes()),
-            Tag::Int { value } => bytes.extend_from_slice(&value.to_be_bytes()),
-            Tag::Long { value } => bytes.extend_from_slice(&value.to_be_bytes()),
-            Tag::Float { value } => bytes.extend_from_slice(&value.to_be_bytes()),
-            Tag::Double { value } => bytes.extend_from_slice(&value.to_be_bytes()),
-            Tag::ByteArray { value } => {
+            Tag::Byte(value) => bytes.extend_from_slice(&value.to_be_bytes()),
+            Tag::Short(value) => bytes.extend_from_slice(&value.to_be_bytes()),
+            Tag::Int(value) => bytes.extend_from_slice(&value.to_be_bytes()),
+            Tag::Long(value) => bytes.extend_from_slice(&value.to_be_bytes()),
+            Tag::Float(value) => bytes.extend_from_slice(&value.to_be_bytes()),
+            Tag::Double(value) => bytes.extend_from_slice(&value.to_be_bytes()),
+            Tag::ByteArray(value) => {
                 bytes.extend_from_slice(&(value.len() as i32).to_be_bytes());
                 bytes.extend(value.iter().map(|b| *b as u8));
             }
-            Tag::String { value } => {
+            Tag::String(value) => {
                 bytes.extend_from_slice(&(value.len() as u16).to_be_bytes());
                 bytes.extend_from_slice(value.as_bytes());
             }
-            Tag::List { tag_type, value } => {
+            Tag::List(tag_type, value) => {
                 bytes.push(*tag_type);
                 bytes.extend_from_slice(&(value.len() as i32).to_be_bytes());
                 for tag in value {
                     tag.write_bytes(bytes);
                 }
             }
-            Tag::Compound { value } => write_compound_tags_bytes(value, bytes),
-            Tag::IntArray { value } => {
+            Tag::Compound(value) => write_compound_tags_bytes(value, bytes),
+            Tag::IntArray(value) => {
                 bytes.extend_from_slice(&(value.len() as i32).to_be_bytes());
                 for int in value {
                     bytes.extend_from_slice(&int.to_be_bytes());
                 }
             }
-            Tag::LongArray { value } => {
+            Tag::LongArray(value) => {
                 bytes.extend_from_slice(&(value.len() as i32).to_be_bytes());
                 for long in value {
                     bytes.extend_from_slice(&long.to_be_bytes());
@@ -122,13 +129,14 @@ impl Tag {
     }
 }
 
-fn write_compound_tags_bytes(tags: &HashMap<String, Tag>, bytes: &mut Vec<u8>) {
+fn write_compound_tags_bytes(tags: &Vec<(String, Tag)>, bytes: &mut Vec<u8>) {
     for (name, tag) in tags {
-        bytes.push(tag.tag_type());
-        write_tag_name_bytes(name, bytes);
+        bytes.push(tag.tag_type_id());
+        if tag.tag_type_id() != TAG_END {
+            write_tag_name_bytes(name, bytes);
+        }
         tag.write_bytes(bytes);
     }
-    bytes.push(TAG_END);
 }
 
 fn write_tag_name_bytes(name: &str, bytes: &mut Vec<u8>) {
