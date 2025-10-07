e3f41e7286ff8996a786150e1c8edc38beccc9a1
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-07 18:18:26 +0200

    Add clientbound login packet skeletons

--- src/mc/packet/client/login.rs
+++ src/mc/packet/client/login.rs
@@ -5,8 +5,30 @@ use crate::mc::protocol::BytesMutExt;
 use crate::server::game_profile::{GameProfile, GameProfileProperty};
 
 #[derive(Debug)]
+pub struct Disconnect;
+
+impl ClientboundPacket for Disconnect {
+    const PACKET_ID: i32 = 0x00;
+
+    fn encode_data(self, _bytes: &mut BytesMut) {
+        todo!();
+    }
+}
+
+#[derive(Debug)]
+pub struct EncryptionRequest;
+
+impl ClientboundPacket for EncryptionRequest {
+    const PACKET_ID: i32 = 0x01;
+
+    fn encode_data(self, _bytes: &mut BytesMut) {
+        todo!();
+    }
+}
+
+#[derive(Debug)]
 pub struct LoginSuccess {
-    pub(crate) game_profile: GameProfile,
+    pub game_profile: GameProfile,
 }
 
 impl ClientboundPacket for LoginSuccess {
@@ -30,5 +52,38 @@ impl GameProfileProperty {
         bytes.put_prefixed_string(&self.name);
         bytes.put_prefixed_string(&self.value);
         bytes.put_prefixed_option(&self.signature, |sig, bytes| bytes.put_prefixed_string(sig));
+    }
+}
+
+#[derive(Debug)]
+pub struct SetCompression;
+
+impl ClientboundPacket for SetCompression {
+    const PACKET_ID: i32 = 0x03;
+
+    fn encode_data(self, _bytes: &mut BytesMut) {
+        todo!();
+    }
+}
+
+#[derive(Debug)]
+pub struct LoginPluginRequest;
+
+impl ClientboundPacket for LoginPluginRequest {
+    const PACKET_ID: i32 = 0x04;
+
+    fn encode_data(self, _bytes: &mut BytesMut) {
+        todo!();
+    }
+}
+
+#[derive(Debug)]
+pub struct CookieRequest;
+
+impl ClientboundPacket for CookieRequest {
+    const PACKET_ID: i32 = 0x05;
+
+    fn encode_data(self, _bytes: &mut BytesMut) {
+        todo!();
     }
 }
