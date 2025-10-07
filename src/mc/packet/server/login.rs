debba8d16839b29c705cc2eef14f78d152313ff0
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-07 18:13:30 +0200

    Send LoginSuccess packet and update connection state on login

--- src/mc/packet/server/login.rs
+++ src/mc/packet/server/login.rs
@@ -2,10 +2,10 @@ use bytes::Bytes;
 use uuid::Uuid;
 
 use crate::error::KeisteenResult;
-use crate::mc::packet::ServerboundRawPacket;
 use crate::mc::packet::server::ServerboundPacket;
+use crate::mc::packet::{self, ServerboundRawPacket};
 use crate::mc::protocol::BytesExt;
-use crate::server::connection::Connection;
+use crate::server::connection::{Connection, ConnectionState};
 use crate::server::game_profile::GameProfile;
 
 #[derive(Debug)]
@@ -24,14 +24,16 @@ impl ServerboundPacket for LoginStart {
     async fn handle(self, connection: &mut Connection) -> KeisteenResult<()> {
         log::trace!("<<< {self:?}");
 
-        let profile = GameProfile {
+        let game_profile = GameProfile {
             uuid: self.player_uuid,
             username: self.name,
             // TODO: load actual properties.
             properties: vec![],
         };
 
-        connection.server().add_game_profile(profile);
+        connection.server().add_game_profile(game_profile.clone());
+
+        connection.send_packet(packet::client::login::LoginSuccess { game_profile }).await?;
 
         Ok(())
     }
@@ -55,6 +57,7 @@ impl ServerboundPacket for EncryptionResponse {
 
     async fn handle(self, _connection: &mut Connection) -> KeisteenResult<()> {
         log::trace!("<<< {self:?}");
+
         Ok(())
     }
 }
@@ -84,8 +87,11 @@ impl ServerboundPacket for LoginAcknowledged {
         Ok(Self {})
     }
 
-    async fn handle(self, _connection: &mut Connection) -> KeisteenResult<()> {
+    async fn handle(self, connection: &mut Connection) -> KeisteenResult<()> {
         log::trace!("<<< {self:?}");
+
+        connection.set_state(ConnectionState::Config);
+
         Ok(())
     }
 }
