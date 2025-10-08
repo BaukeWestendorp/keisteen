eb027689d8fc631da550a2e0208490f99291ed22
Bauke Westendorp <baukewestendorp@gmail.com>
2025-10-08 21:15:32 +0200

    Add `DimensionType`

--- src/mc/registries/mod.rs
+++ src/mc/registries/mod.rs
@@ -6,6 +6,7 @@ use crate::mc::resources::ResourceLocation;
 use crate::mc::sounds::SoundEvent;
 use crate::mc::text::TextComponent;
 use crate::mc::types::Identifier;
+use crate::mc::util::IntProvider;
 use crate::mc::world::entity::SpawnPrioritySelectors;
 
 pub use item::*;
@@ -23,6 +24,7 @@ pub struct Registries {
     cow_variants: BTreeMap<ResourceLocation, RegItem<CowVariant>>,
     damage_type: BTreeMap<ResourceLocation, RegItem<DamageType>>,
 
+    dimension_types: BTreeMap<ResourceLocation, RegItem<DimensionType>>,
     frog_variants: BTreeMap<ResourceLocation, RegItem<FrogVariant>>,
     painting_variants: BTreeMap<ResourceLocation, RegItem<PaintingVariant>>,
     pig_variants: BTreeMap<ResourceLocation, RegItem<PigVariant>>,
@@ -41,6 +43,7 @@ impl Registries {
         let cow_variants = CowVariant::load_from_file();
         let damage_type = DamageType::load_from_file();
 
+        let dimension_types = DimensionType::load_from_file();
         let frog_variants = FrogVariant::load_from_file();
         let painting_variants = PaintingVariant::load_from_file();
         let pig_variants = PigVariant::load_from_file();
@@ -57,6 +60,7 @@ impl Registries {
             cow_variants,
             damage_type,
 
+            dimension_types,
             frog_variants,
             painting_variants,
             pig_variants,
@@ -89,6 +93,10 @@ impl Registries {
 
     pub fn damage_types(&self) -> &BTreeMap<ResourceLocation, RegItem<DamageType>> {
         &self.damage_type
+    }
+
+    pub fn dimension_types(&self) -> &BTreeMap<ResourceLocation, RegItem<DimensionType>> {
+        &self.dimension_types
     }
 
     pub fn frog_variants(&self) -> &BTreeMap<ResourceLocation, RegItem<FrogVariant>> {
@@ -310,7 +318,43 @@ pub enum DamageEffects {
 
 // TODO: Dialog
 
-// TODO: DimensionType
+#[derive(Debug)]
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct DimensionType {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fixed_time: Option<i64>,
+    pub has_skylight: bool,
+    pub has_ceiling: bool,
+    pub ultrawarm: bool,
+    pub natural: bool,
+    pub coordinate_scale: f64,
+    pub bed_works: bool,
+    pub respawn_anchor_works: bool,
+    pub min_y: i32,
+    pub height: i32,
+    pub logical_height: i32,
+    pub infiniburn: String,
+    pub effects: ResourceLocation,
+    pub ambient_light: f32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cloud_height: Option<i32>,
+    #[serde(flatten)]
+    pub monster_settings: Option<MonsterSettings>,
+}
+
+impl Registry for DimensionType {
+    fn identifier() -> Identifier {
+        Identifier::new("minecraft", "dimension_type").unwrap()
+    }
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct MonsterSettings {
+    pub piglin_safe: bool,
+    pub has_raids: bool,
+    pub monster_spawn_light_level: IntProvider,
+    pub monster_spawn_block_light_limit: i32,
+}
 
 #[derive(Debug)]
 #[derive(serde::Serialize, serde::Deserialize)]
--- src/mc/util.rs
+++ src/mc/util.rs
@@ -1,3 +1,27 @@
 pub trait Predicate<T> {
     fn test(&self, t: &T) -> bool;
 }
+
+#[derive(Debug)]
+#[derive(serde::Serialize, serde::Deserialize)]
+pub enum IntProvider {
+    #[serde(rename = "minecraft:constant")]
+    Constant { value: i32 },
+    #[serde(rename = "minecraft:uniform")]
+    Uniform { min_inclusive: i32, max_inclusive: i32 },
+    #[serde(rename = "minecraft:biased_to_bottom")]
+    BiasedToBottom { min_inclusive: i32, max_inclusive: i32 },
+    #[serde(rename = "minecraft:clamped")]
+    Clamped { min_inclusive: i32, max_inclusive: i32, source: Box<IntProvider> },
+    #[serde(rename = "minecraft:clamped_normal")]
+    ClampedNormal { mean: f64, deviation: f64, min_inclusive: i32, max_inclusive: i32 },
+    #[serde(rename = "minecraft:weighted_list")]
+    WeigthedList(Vec<WeightedEntry<i32>>),
+}
+
+#[derive(Debug)]
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct WeightedEntry<T> {
+    pub data: T,
+    pub weight: i32,
+}
